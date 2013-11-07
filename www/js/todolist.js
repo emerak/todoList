7f398e9568f71e697a3ca800883c533b7658a703
Stiffen Alejandro Macias Quintero <stiffen.macias@tangosource.com>
2013-11-07 12:24:52 -0600

    Added functionality to delete a specific task

--- www/js/todolist.js
+++ www/js/todolist.js
@@ -1,4 +1,17 @@
+tasks_count = 0
 $('.button-task').on('click',function(){
- var task = $(".text-task").val();
- $(".to-do-list").append("<p>"+task+"</p>");
-});
+  tasks_count = tasks_count+1;  
+  var task = $(".text-task").val();
+  $(".to-do-list").append("<div class="+tasks_count+"><p>"+task+"</p><button class='delete-task' id="+tasks_count+">X</button></div>");
+  debugger
+  delete_task();
+ })
+
+ var delete_task = function(){ 
+  $('.delete-task').on('click',function(e){
+    task_num = e.target.id;
+    $("."+task_num+"").remove();
+  });
+ }
+
+
