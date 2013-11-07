447882ad44157b4d87c5d02c905aa4a8156092f3
Stiffen Alejandro Macias Quintero <stiffen.macias@tangosource.com>
2013-11-07 13:21:54 -0600

    Added function edit_task

--- www/js/todolist.js
+++ www/js/todolist.js
@@ -2,15 +2,22 @@ tasks_count = 0
 $('.button-task').on('click',function(){
   tasks_count = tasks_count+1;  
   var task = $(".text-task").val();
-  $(".to-do-list").append("<div class="+tasks_count+"><p class="+tasks_count+">"+task+"</p><button class='delete-task' id="+tasks_count+">X</button></div>");
+  $(".to-do-list").append("<div class='task' id="+tasks_count+"><p id="+tasks_count+">"+task+"</p><button class='edit-task'id="+tasks_count+">Edit</button><button class='delete-task' id="+tasks_count+">X</button></div>");
   delete_task();
- })
+  edit_task();
+ });
 
  var delete_task = function(){ 
   $('.delete-task').on('click',function(e){
     task_num = e.target.id;
-    $("."+task_num+"").remove();
+    $("#"+task_num+"").remove();
   })
+ };
+
+ var edit_task = function(){
+   $(".edit-task").on('click',function(e){
+
+   }) 
  };
 
 
