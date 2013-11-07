7f58eadaeb5117bd106c256723a0261db3bd730e
Stiffen Alejandro Macias Quintero <stiffen.macias@tangosource.com>
2013-11-07 12:35:06 -0600

    Deleted debugger

--- www/js/todolist.js
+++ www/js/todolist.js
@@ -2,8 +2,7 @@ tasks_count = 0
 $('.button-task').on('click',function(){
   tasks_count = tasks_count+1;  
   var task = $(".text-task").val();
-  $(".to-do-list").append("<div class="+tasks_count+"><p>"+task+"</p><button class='delete-task' id="+tasks_count+">X</button></div>");
-  debugger
+  $(".to-do-list").append("<div class="+tasks_count+"><p class="+tasks_count+">"+task+"</p><button class='delete-task' id="+tasks_count+">X</button></div>");
   delete_task();
  })
 
@@ -11,7 +10,7 @@ $('.button-task').on('click',function(){
   $('.delete-task').on('click',function(e){
     task_num = e.target.id;
     $("."+task_num+"").remove();
-  });
- }
+  })
+ };
 
 
