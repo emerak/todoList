cc4d1360d51ecf7c5d5cb1f266100760b1ad58ce
Stiffen Alejandro Macias Quintero <stiffen.macias@tangosource.com>
2013-11-07 22:30:38 -0600

    Added Basic Template for app

--- www/js/todolist.js
+++ www/js/todolist.js
@@ -1,41 +1,8 @@
-tasks_count = 0
-$('.button-task').on('click',function(){
-  tasks_count = tasks_count+1;  
-  var task = $(".text-task").val();
-  $(".to-do-list").append("<div class="+tasks_count+"><p class="+tasks_count+">"+task+"</p><input class='hidden' type='text' value="+task+"></input><button class='edit-task' id="+tasks_count+">Edit</button><button class='delete-task' id="+tasks_count+">Delete</button></div>");
-  $(".text-task").val("");
-  edit_task();
-  delete_task();
- })
+window.kendoMobileApplication = new kendo.mobile.Application(document.body);
 
- var delete_task = function(){ 
-  $('.delete-task').on('click',function(e){
-    task_num = e.target.id;
-    $("."+task_num+"").remove();
-  })
- };
- 
- var edit_task = function(){
-   $(".edit-task").click(function(e){
-     var $task = $(this).parent();
-     var $edit = $task.find(".edit-task");
-     $task.find("p").addClass("hidden");
-     $task.find("input").removeClass("hidden");
-     $edit.toggleClass("save-task");
-     $edit.text("Save");
-     save_task();
-   });
- }
-
-var save_task = function(){
-  $(".save-task").click(function(){
-    $task = $(this).parent().find("p")
-    $task.text($(this).prev().val())
-    $task.removeClass("hidden");
-    $(this).toggleClass("save-task");
-    $(this).text("Edit");
-    $(this).prev().addClass("hidden");
-    edit_task();
-
+var app = new kendo.mobile.Application(document.body, 
+  {
+    platform:'android'
   });
-}
+
+
